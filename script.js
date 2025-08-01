e2678b3abd0e31b11d005a3f14684d4895e9fb99
Anirudh Vainatheya <anirudhstarc7@gmail.com>
2025-08-01 06:12:51 +0530

    new recaptcha changes

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function beforesubmit() {
+let captchachekced = false;
+if (captchachekced) {
+    function beforesubmit() {
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log(inputdate.value); 
 
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
     outputdate.value = formattedDate;
+    }
+} else {
+    alert("Please check the RECAPTCHA");
+    event.preventDefault();
 }
 
+
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function captchasuccess() {
+    captchachekced = true;
+}
